22e216f914ce25a35be57d220a78c684cc1f9bf2
Simeon-Azeh <s.kongnyuy@alustudent.com>
2023-11-07 00:34:35 -0800

    g

--- python-data_structures/12-switch.py
+++ python-data_structures/12-switch.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 a = 89
 b = 10
-a, b = b, a
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+
+# Switch the values of a and b
+temp = a
+a = b
+b = temp
+
 print("a={:d} - b={:d}".format(a, b))

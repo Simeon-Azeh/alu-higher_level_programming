29068e5bd862d833d2b60118f212f0087c667810
Simeon-Azeh <s.kongnyuy@alustudent.com>
2023-11-14 08:05:29 -0800

    f

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,25 @@
 #!/usr/bin/python3
 
+"""
+This module defines the Square class.
+
+The Square class is a simple class representing a square.
+
+Example:
+>>> square_instance = Square()
+>>> print(type(square_instance))
+<class '__main__.Square'>
+"""
+
 class Square:
+    """
+    A simple class representing a square.
+
+    This class doesn't have any attributes or methods.
+
+    Example:
+    >>> square_instance = Square()
+    >>> print(type(square_instance))
+    <class '__main__.Square'>
+    """
     pass

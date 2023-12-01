5b2c83b77b78d1865fd46fe7795c5ad6d210f802
Simeon-Azeh <s.kongnyuy@alustudent.com>
2023-12-01 00:19:08 -0800

    d

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a GET request to the specified URL and display the body of the response
-[ "$#" -ne 1 ] && exit 1; curl -s -w "\n" "$1"
+[ "$#" -ne 1 ] && echo "Direct access" && exit 1; curl -s -w "\n" "$1"

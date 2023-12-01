f1fb858a1bf2f7fa0e4b8e555f8ce45ee74d99ac
Simeon-Azeh <s.kongnyuy@alustudent.com>
2023-11-30 23:41:55 -0800

    more

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,8 +1,4 @@
--- Creates database
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
--- Use database 
-USE hbtn_0d_usa;
-
--- Creates a table within the database
-CREATE TABLE IF NOT EXISTS states(id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL PRIMARY KEY(id));
+-- Write a script that creates the database hbtn_0d_usa and the table states on your MySQL server.
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+USE `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `states` (id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256));

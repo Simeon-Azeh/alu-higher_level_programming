e067f8f551b5820402f6c2621a80558f8fed4575
Simeon-Azeh <144020513+Simeon-Azeh@users.noreply.github.com>
2024-01-30 10:56:16 +0100

    fixed

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
-const myVar = "Javascript is amazing";
 
+// Create a constant variable called myVar
+const myVar = "JavaScript is amazing";
+
+// Use console.log(...) to print the output
 console.log(myVar);

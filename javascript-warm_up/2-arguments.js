efd87f0f4a78ace0d0ec5b90e0549e969f351efa
Simeon-Azeh <144020513+Simeon-Azeh@users.noreply.github.com>
2024-01-31 09:20:31 +0100

    Fixed bugs

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,12 +1,11 @@
-#!/usr/bin/node 
-    const NumArguments = process.argv.length - 2;
-//print message based on number of arguments 
+#!/usr/bin/node
+const NumArguments = process.argv.length - 2;
 
-    if (NumArguments === 0) {
-    console.log('No argument');
-} else if(NumArguments === 1) {
-    console.log('Argument found');
+// Print message based on the number of arguments
+if (NumArguments === 0) {
+  console.log('No argument');
+} else if (NumArguments === 1) {
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
-
+  console.log('Arguments found');
 }

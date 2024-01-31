9d4281c6be0619cc2e430d2e86aca5fe75e1528c
Simeon-Azeh <144020513+Simeon-Azeh@users.noreply.github.com>
2024-01-31 09:55:56 +0100

    Javvv

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-
-// Array of strings
-const languages = ['C is fun','Python is cool','JavaScript is amazing'];
-  // Loop through the array and print each line
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const language of languages) {
     console.log(language);
 }
-  
+console.log('\n')

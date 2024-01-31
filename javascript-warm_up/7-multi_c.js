fdc638cc556007c4e11ba50daa042376e310d7b9
Simeon-Azeh <144020513+Simeon-Azeh@users.noreply.github.com>
2024-01-31 10:00:56 +0100

    jaja

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -4,10 +4,10 @@
 const arg = process.argv[2];
 
 // Convert the argument to an integer
-const num = parseInt(arg);
+const num = !isNaN(parseInt(arg));
 
 // Check if the conversion was successful and the number is positive
-if (!isNaN(num) && num > 0) {
+if (num) {
   // Loop through and print "C is fun" x times
   for (let i = 0; i < num; i++) {
     console.log('C is fun');

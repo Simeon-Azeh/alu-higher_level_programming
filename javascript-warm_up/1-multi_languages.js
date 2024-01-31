2e07ef3acc3395493f7f661b627e60bbbe5a6cc5
Simeon-Azeh <144020513+Simeon-Azeh@users.noreply.github.com>
2024-01-31 09:04:59 +0100

    Finished

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,10 @@
+#!/usr/bin/node
+
+// javascript-warm_up\1-multi_languages.js
 const a = 'C is fun';
 const b = 'Python is cool';
 const c = 'JavaScript is amazing';
-
+// Use console.log(...) to print the output
 console.log(a);
 console.log(b);
 console.log(c);
